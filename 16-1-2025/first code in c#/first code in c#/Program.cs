4c78ff87339a24e0a6fba21d5d8740a0d2f90f72
halawardeh <halawardeh12102001@gmail.com>
2025-01-16 17:02:33 +0300

    complete the all 5 tasks

--- 16-1-2025/first code in c#/first code in c#/Program.cs
+++ 16-1-2025/first code in c#/first code in c#/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
 
 
 namespace first_code_in_c_
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
 
@@ -45,9 +49,17 @@ namespace first_code_in_c_
             //const double number= 141088;
             //Console.WriteLine(number);
 
+            //task #3 : 	Define array with name car that hold more than 3 kinds on cars, then print all the cars with the array length.
+
+            //string[] cars = { "Kia", "Volvo", "Honday" };
+            //foreach (string car in cars) {
+            //    Console.WriteLine(car);
+            //}
 
 
-            //task #1 : 	Write a program in C that reads a first name, Lastname and year of birth
+
+
+            //task #4 : 	Write a program in C that reads a first name, Lastname and year of birth
             //and display the names and the year one after another sequentially. Go to the editor
 
             //•	Input your firstname: Sami
@@ -55,14 +67,42 @@ namespace first_code_in_c_
             //•	Input your year of birth: 1999
             //•	Sami Ali 1999
 
-            string fname;
-            fname = Console.ReadLine();
+            //string fname, lname, birthdate;
+            //Console.WriteLine("Input your firstname:");
+            //fname = Console.ReadLine();
+
+            //Console.WriteLine("Input your lastname:");
+            //lname = Console.ReadLine();
+
+            //Console.WriteLine("Input your year of birth year:");
+            //birthdate = Console.ReadLine();
+
+            //Console.WriteLine(fname + " " + lname + " " + birthdate);
+
+            //task #5 : 	Write a program in C # to store elements in an array and print it. 
+            //•	Test Data :	
+            //•	Input 10 elements in the array :
+            //•	element - 0 : 1
+            //•	element - 1 : 1
+            //•	element - 2 : 2
+            //•	.......
+            //•	Expected Output :
+            //•	Elements in array are: 1 1 2 3 4 5 6 7 8 9
+            //•	Elements in array are: 1 1 2 3 4 5 6 7 8 9
 
 
+            Console.WriteLine("Input 10 elements in the array :");
 
+            int[] numbers = new int[10];
+            
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
-
+            for (int i = 0; i < numbers.Length; i++)
+                Console.Write(numbers[i] + " ");
         }
     }
 }
