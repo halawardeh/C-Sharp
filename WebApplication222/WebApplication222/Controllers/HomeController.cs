5756366b66e64153e33dff50c829dd65452bf867
halawardeh <halawardeh12102001@gmail.com>
2025-02-16 23:09:50 +0300

    .net

---
+++ WebApplication222/WebApplication222/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication222.Models;
+
+namespace WebApplication222.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ home2/home2/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="home2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div class=" mb-3">
+
+                <label for="exampleInputForm" class="form-label">ID</label>
+                <asp:TextBox runat="server" ID="BookID" aria-describedby="emailHelp" class="form-constrol"></asp:TextBox>
+                <asp:Button runat="server" ID="addBook" Text="Add Book" OnClick="addbook_click" />
+
+            </div>
+        </div>
+    </form>
+</body>
+</html>
